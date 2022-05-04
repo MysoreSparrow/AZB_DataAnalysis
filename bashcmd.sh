86b7d3861c715c00bc45b47380adf6dc3a5e5448
MysoreSparrow <98920181+MysoreSparrow@users.noreply.github.com>
2022-05-04 18:43:06 +0200

    Add files via upload

--- bashcmd.sh
+++ bashcmd.sh
@@ -33,7 +33,7 @@ multiqc . -o /root/alina_rnaseq/fastq/output/
 cd /root/alina_rnaseq/fastq/
 
 for f in *.fastq.gz; do cutadapt -j 4 -a "poly A=A{20}" --quality-cutoff 20 -u 12 -o /root/alina_rnaseq/cutadapt/trimmed_run4/run4_trimmed_$f $f 1>> /root/alina_rnaseq/reports/report_Cutadapt_run4.txt; done
-
+# also add -m 20
 # Cutadapt parameters:
 # - a: The sequence of the adapter is given with the -a option. You need to replace AACCGGTT with the correct adapter sequence. or also look poly A and poly T sequences. 
 # - q: The -q (or --quality-cutoff) parameter can be used to trim low-quality ends from reads. If you specify a single cutoff value, the 3’ end of each read is trimmed. Used for quality trimming to remove short reads (<20nt).
@@ -90,4 +90,52 @@ STAR  --runThreadN 4 --runMode genomeGenerate --genomeDir /data/keshav/alina_rna
 # genome generation succesfully completed!!!
 
 # Running the mapping job
-STAR --runThreadN 4 --genomeDir /data/keshav/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ --readFilesIn /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/*.fastq.gz --readFilesCommand zcat --outFileNamePrefix /data/keshav/alina_rnaseq/mapping_alignment/star_mapped/starmapped_run1_ --outSAMtype BAM SortedByCoordinate
+STAR --runThreadN 4 --genomeDir /data/keshav/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ 
+    --readFilesIn /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/*.fastq.gz --readFilesCommand zcat 
+    --outFileNamePrefix /data/keshav/alina_rnaseq/mapping_alignment/star_mapped/starmapped_run1_ 
+    --outSAMtype BAM SortedByCoordinate
+
+# for individual files 
+STAR --genomeDir /data/keshav/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ --readFilesIn /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/run5_trimmed_757_S11_R1_001.fastq.gz --readFilesCommand zcat --outFileNamePrefix /data/keshav/alina_rnaseq/mapping_alignment/mapped/run3_ --outSAMtype BAM SortedByCoordinate
+
+
+# for set of trimmed fastq files in a folder
+
+for i in $(ls /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/test/ ); do echo "Processing file ${i}" STAR --genomeDir /data/keshav/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ --readFilesIn /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/test/${i} --outFileNamePrefix /data/keshav/alina_rnaseq/mapping_alignment/mapped/runtestaligned_$i --outSAMtype BAM SortedByCoordinate --sjdbGTFfile /data/keshav/alina_rnaseq/mapping_alignment/mouse_genome_m39/Mus_musculus.GRCm39.106.chr.gtf ; done
+
+for i in $(ls /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/test/ ); do echo "Processing file ${i}" STAR --genomeDir /data/keshav/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ --readFilesIn /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/test/${i} --outFileNamePrefix /data/keshav/alina_rnaseq/mapping_alignment/mapped/runtestaligned_$i ; done
+
+STAR --genomeDir /data/keshav/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ --readFilesIn /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/run5_trimmed_755_S2_R1_001.fastq,/data/keshav/alina_rnaseq/cutadapt/trimmed_run5/run5_trimmed_758_S6_R1_001.fastq --outSAMtype BAM Unsorted SortedByCoordinate
+
+# STAR Local run 
+STAR --genomeDir /root/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ --readFilesIn /root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_476_S14_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/ --outFileNamePrefix /root/alina_rnaseq/mapping_alignment/aligned/run1aligned_ --outSAMtype BAM SortedByCoordinate --sjdbGTFfile /root/alina_rnaseq/mapping_alignment/mouse_genome_m39/Mus_musculus.GRCm39.106.chr.gtf 
+
+# List of files
+/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_758_S6_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_756_S15_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_760_S12_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_764_S7_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_768_S17_R2_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_Ctrl2_S21_R2_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_476_S20_R2_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_757_S11_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_761_S4_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_765_S1_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_768_S8_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_754_S3_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_757_S18_R2_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_762_S5_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_766_S13_R1_001.fastq,/root/alina_rnaseq/mapping_alignment/trimmed_run5/run5_trimmed_769_S9_R1_001.fastq
+
+for i $(ls /root/alina_rnaseq/mapping_alignment/trimmed_run5/ ); do echo $i ; done 
+
+# nano cmd
+
+for i in $(ls /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/test/ ); do echo "#######################Processing file $i ##################################" \
+STAR --genomeDir /data/keshav/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/ --readFilesIn /data/keshav/alina_rnaseq/cutadapt/trimmed_run5/test/$i --quantMode GeneCounts --outFileNamePrefix /data/kesha$
+
+
+bash script fr local run:
+
+#!/bin/bash
+
+# define variables
+gd=/root/alina_rnaseq/mapping_alignment/genome_index_fromSTAR_usingm39/
+# get our data files
+FILES=/root/alina_rnaseq/mapping_alignment/trimmed_run5/
+#   --readFilesCommand zcat
+
+for f in $FILES
+do
+    echo $f
+    STAR --runThreadN 4 --genomeDir $gd --readFilesIn $f --outSAMtype BAM SortedByCoordinate \
+         --quantMode GeneCounts --outFileNamePrefix $mapped"aligned_"
+done
+
+echo "done!"
